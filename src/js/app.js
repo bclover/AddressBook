22509c26909ebe7caa29e854740060893534451c
Bryan Clover <bclover09@gmail.com>
2015-04-02 14:58:23 -0400

    Added watcher to reload browser when javascript files change. Added some sample data, styling, and layout tweaks.

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,13 @@
 /**
  * Created by bryanclover on 4/2/15.
  */
-function PeopleController($scope){
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('PeopleController', ['$scope', function($scope) {
     $scope.people = [
         {name: 'John Doe', phone: '425-218-1123', city: 'New York'},
         {name: 'Jane Doe', phone: '213-734-1903', city: 'Chicago'},
         {name: 'Same Smith', phone: '919-861-1227', city: 'Raleigh'},
         {name: 'Paul Jones', phone: '412-954-4106', city: 'Pittsburgh'}
     ];
-}
+}]);
